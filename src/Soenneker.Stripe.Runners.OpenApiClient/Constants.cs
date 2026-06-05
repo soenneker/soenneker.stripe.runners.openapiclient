8a9ce5a12b1fee65950cf98898af640d5efab23e
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:50 -0500

    xml commenting

--- src/Soenneker.Stripe.Runners.OpenApiClient/Constants.cs
+++ src/Soenneker.Stripe.Runners.OpenApiClient/Constants.cs
@@ -1,6 +1,12 @@
 ﻿namespace Soenneker.Stripe.Runners.OpenApiClient;
 
+/// <summary>
+/// Represents the constants.
+/// </summary>
 public static class Constants
 {
+    /// <summary>
+    /// The library.
+    /// </summary>
     public const string Library = "Soenneker.Stripe.OpenApiClient";
 }
